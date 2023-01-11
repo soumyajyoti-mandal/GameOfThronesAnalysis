c1314f5fdd98e1233b1aa410aba98214b88f1abf
Soumyajyoti Mandal <111740636+soumyajyoti-mandal@users.noreply.github.com>
2023-01-12 00:38:14 +0530

    Update LambdaFunction.py

--- LambdaFunction.py
+++ LambdaFunction.py
@@ -2,28 +2,35 @@ import json
 import boto3
 import csv
 import mysql.connector
+#create boto3 client
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
     # TODO implement
     #print(event)
+    #extract bucket name and file name from event
     bucket = event['Records'][0]['s3']['bucket']['name']
     csvfile = event['Records'][0]['s3']['object']['key']
+    #create file object
     csvfile_obj = s3_client.get_object(Bucket = bucket,Key =csvfile)
+    #read file object
     lines = csvfile_obj['Body'].read().decode('utf-8').split('\r\n')
     lines_list = []
+    #load data from csv file to a list
     for i in csv.DictReader(lines):
         lines_list.append(i.values())
     #print(lines_list)
     try:
+        #connect to AWS RDS
         mydb = mysql.connector.connect(
-                host="gottaskdb.ctydl14ji75t.ap-south-1.rds.amazonaws.com",
-                user="admin",password="Qwer1234", database ='mydatabase')
+                host="hostname",
+                user="username",password="password", database ='mydatabase')
     except mysql.connector.Error as e:
         print("Error while connecting to MySQL", e)
     else:
         mycursor = mydb.cursor()
         try:
+            #load list data to mysql table
             myquery = '''insert into mydatabase.got (name_of_battle , year_of_battle , battle_number,attacker_king , 
             defender_king, attacker_1, attacker_2,attacker_3 ,attacker_4 ,defender_1, 
             defender_2 , defender_3,defender_4, attacker_outcome, battle_type ,major_death ,
